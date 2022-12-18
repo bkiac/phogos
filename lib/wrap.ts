ff0872d2f1517ac135f058bf8096049d7cffbb14
bkiac <bkiac@pm.me>
2022-12-18 13:51:46 +0100

    Add options to wrappers

--- lib/wrap.ts
+++ lib/wrap.ts
@@ -1,12 +1,13 @@
 import type {PromiseResult, Result} from "./core"
+import type {ErrorHandlerOptions} from "./handle-error"
 import {tryCatch, tryCatchAsync} from "./try-catch"
 
 export const wrap =
-	<F extends (...args: any[]) => any>(fn: F) =>
+	<F extends (...args: any[]) => any, E extends Error = Error>(fn: F, options?: ErrorHandlerOptions<E>) =>
 	(...args: Parameters<F>): Result<ReturnType<F>> =>
-		tryCatch(() => fn(...args))
+		tryCatch(() => fn(...args), options)
 
 export const wrapAsync =
-	<F extends (...args: any[]) => Promise<any>>(fn: F) =>
+	<F extends (...args: any[]) => Promise<any>, E extends Error = Error>(fn: F, options?: ErrorHandlerOptions<E>) =>
 	async (...args: Parameters<F>): PromiseResult<Awaited<ReturnType<F>>> =>
-		tryCatchAsync(async () => fn(...args))
+		tryCatchAsync(async () => fn(...args), options)
