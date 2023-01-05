98ea8e2900c43b2cc8823a27ec4a24c939f35c66
bkiac <bkiac@pm.me>
2023-01-05 21:36:33 +0100

    Add string error arg

--- lib/core.ts
+++ lib/core.ts
@@ -3,4 +3,11 @@ export type Err<E extends Error = Error> = [undefined, E]
 export type Result<V, E extends Error = Error> = Ok<V> | Err<E>
 
 export const ok = <V>(value: V): Ok<V> => [value, undefined]
-export const err = <E extends Error>(error: E): Err<E> => [undefined, error]
+
+export function err<E extends Error = Error>(error: E): Err<E>
+export function err(error: string): Err<Error>
+export function err(error: Error | string): Err<Error> {
+	return [undefined, error instanceof Error ? error : new Error(error)]
+}
+
+// export const err = <E extends string | Error>(error: E): Err<E> => [undefined, error]
