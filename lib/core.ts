9a6cd984f0cc8d5ca1bbbc020f01f6b80bfcf274
bkiac <bkiac@pm.me>
2023-01-05 21:48:19 +0100

    Fix ok arg type to allow optional arg

--- lib/core.ts
+++ lib/core.ts
@@ -2,5 +2,10 @@ export type Ok<V> = [V, undefined]
 export type Err<E extends Error = Error> = [undefined, E]
 export type Result<V, E extends Error = Error> = Ok<V> | Err<E>
 
-export const ok = <V>(value: V): Ok<V> => [value, undefined]
+export function ok(value?: undefined): Ok<undefined>
+export function ok<V>(value: V): Ok<V>
+export function ok<V>(value: V): Ok<V> {
+	return [value, undefined]
+}
+
 export const err = <E extends Error>(error: E): Err<E> => [undefined, error]
